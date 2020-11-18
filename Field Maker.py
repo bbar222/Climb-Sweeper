085d7869b68d26af3447625324db76cc583b625c
bbar222 <bbarnefiher@gmail.com>
2020-11-18 17:28:08 -0500

    The console-based minesweeper game is functional.

--- Field Maker.py
+++ Field Maker.py
@@ -2,10 +2,12 @@ import numpy as np
 
 field = np.random.randint(0,101,(8,8))
 
-mine = 'X'
-empty = ' '
 
 tiles = {"mine":-1,"empty":0}
+
+
+
+
 
 for row in range(field.shape[0]):
     for col in range(field.shape[1]):
@@ -17,47 +19,67 @@ for row in range(field.shape[0]):
 
 
 
+def checkAbove(r, c):
+    if r != 0:
+        if c != 0:
+            if field[r - 1][c - 1] == tiles.get("mine"):
+                field[r][c] += 1
+        if field[r - 1][c] == tiles.get("mine"):
+            field[r][c] += 1
+        if c != 7:
+            if field[r - 1][c + 1] == tiles.get("mine"):
+                field[r][c] += 1
+
+
+def checkSides(r,c):
+    if c == 0:
+        if field[r][c + 1] == tiles.get("mine"):
+            field[r][c] += 1
+    elif c == 7:
+        if field[r][c - 1] == tiles.get("mine"):
+            field[r][c] += 1
+    else:
+        if field[r][c - 1] == tiles.get("mine"):
+            field[r][c] += 1
+        if field[r][c + 1] == tiles.get("mine"):
+            field[r][c] += 1
+
+
+def checkBelow(r,c):
+    if r != 7:
+        if c != 0:
+            if field[r + 1][c - 1] == tiles.get("mine"):
+                field[r][c] += 1
+        if field[r + 1][c] == tiles.get("mine"):
+            field[r][c] += 1
+        if c != 7:
+            if field[r + 1][c + 1] == tiles.get("mine"):
+                field[r][c] += 1
+
 
 for row in range(field.shape[0]):
     localMines = 0
     for col in range(field.shape[1]):
 
+       # print("testing coordinates",row,", ",col)
 
         if field[row][col] == tiles.get("mine"):
             # Do not need to check as there is a mine here
+            print("found a mine. skipping")
             localMines = 0
             continue
 
 
+        # Check Above
+        checkAbove(row, col)
+        # Check left and right
+        checkSides(row, col)
+        # Check below
+        checkBelow(row, col)
 
-
-        try:
-            # Check row above
-            if row > -1 and col - 1 > -1 and col + 1 < 8:
-                if field[row-1][col-1] == tiles.get("mine"):
-                    field[row][col] += 1
-                if field[row-1][col] == tiles.get("mine"):
-                    field[row][col] += 1
-                if field[row-1][col+1] == tiles.get("mine"):
-                    field[row][col] += 1
-            # Check left and right
-            if col - 1 > -1 and col + 1 < 8:
-                if field[row][col-1] == tiles.get("mine"):
-                    field[row][col] += 1
-                if field[row][col+1] == tiles.get("mine"):
-                    field[row][col] += 1
-            # Check row below
-            if row + 1 < 8 and col - 1 > -1 and col + 1 < 8:
-                if field[row+1][col-1] == tiles.get("mine"):
-                    field[row][col] += 1
-                if field[row+1][col] == tiles.get("mine"):
-                    field[row][col] += 1
-                if field[row+1][col+1] == tiles.get("mine"):
-                    field[row][col] += 1
-        except IndexError:
-            print("lol")
 # Print the board
-for r in field:
-    for c in r:
-        print(c,end = "\t")
+for ro in field:
+    for co in ro:
+        print(co,end = "\t")
     print()
+
